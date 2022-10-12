e17004d338c89633e8db1469c4ab789c7831d6bf
washtakzo <washtakzo@gmail.com>
2022-10-12 17:53:06 +0200

    scale effect & cursor pointer

--- components/LargCard.tsx
+++ components/LargCard.tsx
@@ -11,33 +11,26 @@ type Props = {
 
 const LargCard: React.FC<Props> = ({ img, title, subtitle, buttonText }) => {
   return (
-    <div>
-      <div className="relative w-auto h-96 m-auto">
-        <Image
-          src={img}
-          layout="fill"
-          objectFit="cover"
-          className="rounded-xl"
-        />
-        <div className="absolute top-20 left-12">
-          <Typography
-            className="text-white w-[250px]"
-            component="h5"
-            variant="h4"
-          >
-            {title}
-          </Typography>
-          <Typography className="text-white mt-2" component="p">
-            {subtitle}
-          </Typography>
-          <Button
-            variant="contained"
-            color="secondary"
-            className="mt-4 text-sm normal-case py-2"
-          >
-            {buttonText}
-          </Button>
-        </div>
+    <div className="relative w-auto h-96 m-auto cursor-pointer hover:scale-105 transition duration-200 ease-out">
+      <Image src={img} layout="fill" objectFit="cover" className="rounded-xl" />
+      <div className="absolute top-20 left-12">
+        <Typography
+          className="text-white w-[250px]"
+          component="h5"
+          variant="h4"
+        >
+          {title}
+        </Typography>
+        <Typography className="text-white mt-2" component="p">
+          {subtitle}
+        </Typography>
+        <Button
+          variant="contained"
+          color="secondary"
+          className="mt-4 text-sm normal-case py-2"
+        >
+          {buttonText}
+        </Button>
       </div>
     </div>
   );

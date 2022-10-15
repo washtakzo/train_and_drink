cc56d2e92117525f75f82f7240eb42c85f632393
washtakzo <washtakzo@gmail.com>
2022-10-15 12:33:08 +0200

    nouveau design banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -6,23 +6,23 @@ const Banner = () => {
   return (
     <div className="relative w-full h-96 sm:h-[400px] md:h-[500px] lg:h-[600px] xl:h-[700px]">
       <Image
-        src="https://www.theanimedaily.com/wp-content/uploads/2022/08/Blue-Lock-Anime-Release-Date2.webp"
+        src="https://web.archive.org/web/20210725183154im_/https://a0.muscache.com/im/pictures/e4a2a61c-589f-4e49-b3b8-968a6bc23389.jpg?im_w=2560"
         layout="fill"
         objectFit="cover"
-        className="saturate-50"
+        className=""
       />
       <div className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] flex flex-col justify-between items-center">
         <Typography
-          className="font-semibold px-2 py-1 bg-[#ffffffdd] rounded-md text-xl flex-grow"
+          className="font-semibold px-2 py-1  rounded-md text-sm md:text-xl flex-grow"
           component="p"
-          color="primary"
+          color="white"
         >
-          Not sure where to go ?
+          Not sure where to train ?
         </Typography>
         <Button
           variant="contained"
-          color="secondary"
-          className="mt-2 normal-case"
+          color="primary"
+          className="mt-2 normal-case font-semibold md:text-xl"
         >
           I am flexible
         </Button>

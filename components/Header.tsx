d0f6f841522f0c2d1888c86eadc8b34cdacb89be
washtakzo <washtakzo@gmail.com>
2022-10-16 20:41:10 +0200

    style mis a jour

--- components/Header.tsx
+++ components/Header.tsx
@@ -62,7 +62,7 @@ const Header: React.FC<Props> = ({ placeholder, canBeTransparent = true }) => {
           <Typography
             variant="h5"
             component="h1"
-            className="py-6 px-4 font-bold cursor-pointer"
+            className="py-6 px-4 font-bold cursor-pointer text-lg sm:text-2xl"
             fontWeight={"bold"}
             color={`${isHeaderTransparent ? "white" : "primary"}`}
             onClick={() => {
@@ -74,14 +74,16 @@ const Header: React.FC<Props> = ({ placeholder, canBeTransparent = true }) => {
 
           {/* midle */}
           <div
-            className={`w-[100%] max-w-lg border-2 rounded-lg m-auto border-solid ${
-              isHeaderTransparent ? "text-white " : borderTheme
+            className={`w-[100%] max-w-lg m-auto ${
+              isHeaderTransparent
+                ? "text-white bg-white-200 border-solidborder-2rounded-lg"
+                : "borderTheme "
             }${transition}`}
           >
             <TextField
               variant="standard"
-              color="primary"
-              placeholder={placeholder ? placeholder : "Start yout search"}
+              color="secondary"
+              placeholder={placeholder ? placeholder : "Search a home gym"}
               inputProps={{
                 className: `w-[100%] pl-2  ${
                   isHeaderTransparent
@@ -108,22 +110,24 @@ const Header: React.FC<Props> = ({ placeholder, canBeTransparent = true }) => {
               Become a host
             </Typography>
             {/* icon planette */}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className={`w-6 h-6  cursor-pointer ${
-                isHeaderTransparent ? "text-white" : "text-slate-500"
-              }${transition}`}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
-              />
-            </svg>
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className={`h-6  cursor-pointer ${
+                  isHeaderTransparent ? "text-white" : "text-slate-500"
+                }${transition}`}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
+                />
+              </svg>
+            </div>
 
             <div
               className={`flex items-center justify-between border-solid border-2 p-1 rounded-full 
